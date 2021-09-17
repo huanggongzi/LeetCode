f398b312bbee069e89b57b71d2cf936bf1137767
hls1793929520 <1793929520@qq.com>
2021-09-18 01:12:05 +0800

    9月18号每日一题：292. Nim 游戏 当N为4的倍数时，必定会输，通过找规律可以得知。

---
+++ src/com/company/everyDay/_918Nim游戏.java
@@ -0,0 +1,17 @@
+package com.company.everyDay;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 你的名字
+ * @Date: 2021/09/18/1:10
+ * @Description:
+ */
+public class _918Nim游戏 {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }
+        return true;
+    }
+}
