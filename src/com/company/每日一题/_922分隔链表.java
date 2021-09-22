ed85ab5ed498e962587fc06f80e239c119d61d29
hls1793929520 <1793929520@qq.com>
2021-09-22 23:58:29 +0800

    9月22号每日一题：725. 分隔链表 注意每个数组存在的数量即可

---
+++ src/com/company/每日一题/_922分隔链表.java
@@ -0,0 +1,51 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 你的名字
+ * @Date: 2021/09/22/23:57
+ * @Description:
+ */
+public class _922分隔链表 {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        int quotient = n / k, remainder = n % k;
+
+        ListNode[] parts = new ListNode[k];
+        ListNode curr = head;
+        for (int i = 0; i < k && curr != null; i++) {
+            parts[i] = curr;
+            int partSize = quotient + (i < remainder ? 1 : 0);
+            for (int j = 1; j < partSize; j++) {
+                curr = curr.next;
+            }
+            ListNode next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+        return parts;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
