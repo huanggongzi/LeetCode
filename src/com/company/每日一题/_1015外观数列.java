e965e360ee099248f987e9931f77e2db76a8c029
hls1793929520 <1793929520@qq.com>
2021-10-15 23:48:32 +0800

    10月14号每日一题:38. 外观数列 模拟

---
+++ src/com/company/每日一题/_1015外观数列.java
@@ -0,0 +1,30 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2021/10/15/23:47
+ * @Description:
+ */
+public class _1015外观数列 {
+    public String countAndSay(int n) {
+        String str = "1";
+        for (int i = 2; i <= n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            int start = 0;
+            int pos = 0;
+
+            while (pos < str.length()) {
+                while (pos < str.length() && str.charAt(pos) == str.charAt(start)) {
+                    pos++;
+                }
+                sb.append(Integer.toString(pos - start)).append(str.charAt(start));
+                start = pos;
+            }
+            str = sb.toString();
+        }
+
+        return str;
+    }
+}
