6be67dd09c4d19ba14ddb67fa40c44a2df4807aa
hls1793929520 <1793929520@qq.com>
2021-11-10 22:05:29 +0800

    11月10号每日一题:495. 提莫攻击 模拟即可 学习Zookeper

---
+++ src/com/company/每日一题/_1110提莫攻击.java
@@ -0,0 +1,26 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2021/11/10/22:04
+ * @Description:
+ */
+public class _1110提莫攻击 {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        // 当前的持续时间
+        if(timeSeries.length == 0){
+            return duration;
+        }
+        int sum = duration;
+        for(int i = 1; i < timeSeries.length; i++){
+            if(timeSeries[i] - timeSeries[i - 1] >= duration){
+                sum = sum + duration;
+            }else{
+                sum = sum + (timeSeries[i] - timeSeries[i - 1]);
+            }
+        }
+        return sum;
+    }
+}
