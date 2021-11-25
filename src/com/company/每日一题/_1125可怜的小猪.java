affd0959f478d95d54c1ac8b726e9e9f08e1f9b9
hls1793929520 <1793929520@qq.com>
2021-11-25 23:53:08 +0800

    11月25号每日一题:458. 可怜的小猪 困难

---
+++ src/com/company/每日一题/_1125可怜的小猪.java
@@ -0,0 +1,42 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2021/11/25/23:52
+ * @Description:
+ */
+public class _1125可怜的小猪 {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        if (buckets == 1) {
+            return 0;
+        }
+        int[][] combinations = new int[buckets + 1][buckets + 1];
+        combinations[0][0] = 1;
+        int iterations = minutesToTest / minutesToDie;
+        int[][] f = new int[buckets][iterations + 1];
+        for (int i = 0; i < buckets; i++) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j <= iterations; j++) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < buckets; i++) {
+            combinations[i][0] = 1;
+            combinations[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                combinations[i][j] = combinations[i - 1][j - 1] + combinations[i - 1][j];
+            }
+            for (int j = 1; j <= iterations; j++) {
+                for (int k = 0; k <= i; k++) {
+                    f[i][j] += f[k][j - 1] * combinations[i][i - k];
+                }
+            }
+            if (f[i][iterations] >= buckets) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
