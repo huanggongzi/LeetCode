61e3e434ad0db2605cbdb6f500c615f88a514ca6
hls1793929520 <1793929520@qq.com>
2021-11-30 23:00:16 +0800

    11月30号每日一题:400. 第 N 位数字

---
+++ src/com/company/每日一题/_1130第N位数字.java
@@ -0,0 +1,22 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2021/11/30/22:59
+ * @Description:
+ */
+public class _1130第N位数字 {
+    public int findNthDigit(int n) {
+        int len = 1;
+        while (len * 9 * Math.pow(10, len - 1) < n) {
+            n -= len * 9 * Math.pow(10, len - 1);
+            len++;
+        }
+        long s = (long) Math.pow(10, len - 1);
+        long x = n / len - 1 + s;
+        n -= (x - s + 1) * len;
+        return n == 0 ? (int) (x % 10) : (int) ((x + 1) / Math.pow(10, len - n) % 10);
+    }
+}
