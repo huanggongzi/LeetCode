c9d27ee5d209f9a9a3410be84cf3b307b7d8ddfb
hls1793929520 <1793929520@qq.com>
2021-12-21 00:10:23 +0800

    12月21号每日一题:1154. 一年中的第几天,redis设计与实现

---
+++ src/com/company/每日一题/_1221一年中的第几天.java
@@ -0,0 +1,27 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2021/12/21/0:09
+ * @Description:
+ */
+public class _1221一年中的第几天 {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            ++amount[1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+}
