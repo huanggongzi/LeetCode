e57b44ceae64f7b50e204cba587e21019c6bcbb0
hls1793929520 <1793929520@qq.com>
2022-01-01 22:17:20 +0800

    01月01号每日一题:2022. 将一维数组转变成二维数组

---
+++ src/com/company/每日一题/_20220101将一维数组转变成二维数组.java
@@ -0,0 +1,24 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/01/01/22:15
+ * @Description:
+ */
+public class _20220101将一维数组转变成二维数组 {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[0][0];
+        }
+        int num = 0;
+        int[][] array = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = original[num++];
+            }
+        }
+        return array;
+    }
+}
