999936a2be8c778617464272aefbf2f664b90b59
hls1793929520 <1793929520@qq.com>
2022-01-10 23:48:14 +0800

    01月10号每日一题:306. 累加数

---
+++ src/com/company/每日一题/_20220110累加数.java
@@ -0,0 +1,70 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/01/10/23:47
+ * @Description:
+ */
+public class _20220110累加数 {
+    public boolean isAdditiveNumber(String num) {
+        int n = num.length();
+        for (int secondStart = 1; secondStart < n - 1; ++secondStart) {
+            if (num.charAt(0) == '0' && secondStart != 1) {
+                break;
+            }
+            for (int secondEnd = secondStart; secondEnd < n - 1; ++secondEnd) {
+                if (num.charAt(secondStart) == '0' && secondStart != secondEnd) {
+                    break;
+                }
+                if (valid(secondStart, secondEnd, num)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean valid(int secondStart, int secondEnd, String num) {
+        int n = num.length();
+        int firstStart = 0, firstEnd = secondStart - 1;
+        while (secondEnd <= n - 1) {
+            String third = stringAdd(num, firstStart, firstEnd, secondStart, secondEnd);
+            int thirdStart = secondEnd + 1;
+            int thirdEnd = secondEnd + third.length();
+            if (thirdEnd >= n || !num.substring(thirdStart, thirdEnd + 1).equals(third)) {
+                break;
+            }
+            if (thirdEnd == n - 1) {
+                return true;
+            }
+            firstStart = secondStart;
+            firstEnd = secondEnd;
+            secondStart = thirdStart;
+            secondEnd = thirdEnd;
+        }
+        return false;
+    }
+
+    public String stringAdd(String s, int firstStart, int firstEnd, int secondStart, int secondEnd) {
+        StringBuffer third = new StringBuffer();
+        int carry = 0, cur = 0;
+        while (firstEnd >= firstStart || secondEnd >= secondStart || carry != 0) {
+            cur = carry;
+            if (firstEnd >= firstStart) {
+                cur += s.charAt(firstEnd) - '0';
+                --firstEnd;
+            }
+            if (secondEnd >= secondStart) {
+                cur += s.charAt(secondEnd) - '0';
+                --secondEnd;
+            }
+            carry = cur / 10;
+            cur %= 10;
+            third.append((char) (cur + '0'));
+        }
+        third.reverse();
+        return third.toString();
+    }
+}
