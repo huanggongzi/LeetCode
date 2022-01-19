d45c652c11c9392a2b7b75c9c1a940563b9422aa
hls1793929520 <1793929520@qq.com>
2022-01-19 23:31:39 +0800

    01月19号每日一题:219. 存在重复元素 II

---
+++ src/com/company/每日一题/_20220119存在重复元素.java
@@ -0,0 +1,26 @@
+package com.company.每日一题;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/01/19/23:30
+ * @Description:
+ */
+public class _20220119存在重复元素 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
