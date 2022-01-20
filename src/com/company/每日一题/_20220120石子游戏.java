1e75707fc2aacf047b055a4783ab9610b77d18f9
hls1793929520 <1793929520@qq.com>
2022-01-20 23:29:02 +0800

    01月20号每日一题:2029. 石子游戏 IX

---
+++ src/com/company/每日一题/_20220120石子游戏.java
@@ -0,0 +1,28 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/01/20/23:27
+ * @Description:
+ */
+public class _20220120石子游戏 {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+    }
+}
