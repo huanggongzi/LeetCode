936a7b394e6e02a17a22f795f0f91ac8f8129d3b
hls1793929520 <1793929520@qq.com>
2022-01-22 23:42:56 +0800

    01月22号每日一题:1332. 删除回文子序列

---
+++ src/com/company/每日一题/_20220122删除回文子序列.java
@@ -0,0 +1,20 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/01/22/23:42
+ * @Description:
+ */
+public class _20220122删除回文子序列 {
+    public int removePalindromeSub(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return 2;
+            }
+        }
+        return 1;
+    }
+}
