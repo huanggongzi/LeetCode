a1aab995fcc8b9b4b7f794976af21aecf618d97b
hls1793929520 <1793929520@qq.com>
2022-02-18 23:42:47 +0800

    02月18号每日一题:1791. 找出星型图的中心节点,kafka

---
+++ src/com/company/每日一题/_20220218找出星型图的中心节点.java
@@ -0,0 +1,25 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/02/18/23:42
+ * @Description:
+ */
+public class _20220218找出星型图的中心节点 {
+    public int findCenter(int[][] edges) {
+        int len = edges.length + 2;
+        int[] arr = new int[len];
+        for(int[] edge : edges){
+            arr[edge[0]]++;
+            arr[edge[1]]++;
+        }
+        for(int i = 0; i < len; i++){
+            if(arr[i] == len - 2){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
