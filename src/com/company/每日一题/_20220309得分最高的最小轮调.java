ad555c9ee0ef22e32f520703c0e40b744232ef78
hls1793929520 <1793929520@qq.com>
2022-03-09 23:52:33 +0800

    03月09号每日一题:798. 得分最高的最小轮调,Kafka生产者

---
+++ src/com/company/每日一题/_20220309得分最高的最小轮调.java
@@ -0,0 +1,35 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/03/09/23:52
+ * @Description:
+ */
+public class _20220309得分最高的最小轮调 {
+    public int bestRotation(int[] nums) {
+        int n = nums.length;
+        int[] diffs = new int[n];
+        for (int i = 0; i < n; i++) {
+            int low = (i + 1) % n;
+            int high = (i - nums[i] + n + 1) % n;
+            diffs[low]++;
+            diffs[high]--;
+            if (low >= high) {
+                diffs[0]++;
+            }
+        }
+        int bestIndex = 0;
+        int maxScore = 0;
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            score += diffs[i];
+            if (score > maxScore) {
+                bestIndex = i;
+                maxScore = score;
+            }
+        }
+        return bestIndex;
+    }
+}
