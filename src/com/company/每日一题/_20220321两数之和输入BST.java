b602e49247208e95c25dba764c8c0b114a5cba5e
hls1793929520 <1793929520@qq.com>
2022-03-21 23:58:29 +0800

    03月22号每日一题:653. 两数之和 IV - 输入 BST,kafka源码环境搭建

---
+++ src/com/company/每日一题/_20220321两数之和输入BST.java
@@ -0,0 +1,30 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/03/21/23:57
+ * @Description:
+ */
+public class _20220321两数之和输入BST {
+    boolean flag = false;
+
+    public boolean findTarget(TreeNode root, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        dfs(root, k, map);
+        return flag;
+    }
+
+    public void dfs(TreeNode root, int k, Map<Integer, Integer> map) {
+        if (root != null && !flag) {
+            dfs(root.left, k, map);
+            if (map.containsKey(k - root.val)) {
+                flag = true;
+            } else {
+                map.put(root.val, 1);
+            }
+            dfs(root.right, k, map);
+        }
+    }
+}
