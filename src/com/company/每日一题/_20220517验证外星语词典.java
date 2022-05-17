c4dd8aa23f3d0349c7f9de38ed9a66db9c1a1697
hls1793929520 <1793929520@qq.com>
2022-05-17 23:43:12 +0800

    05月17号每日一题:953. 验证外星语词典

---
+++ src/com/company/每日一题/_20220517验证外星语词典.java
@@ -0,0 +1,47 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/05/17/23:42
+ * @Description:
+ */
+public class _20220517验证外星语词典 {
+    HashMap<Character, Integer> map = new HashMap<>();
+    public boolean isAlienSorted(String[] words, String order) {
+        int index = order.length() - 1;
+        for(int i = 0; i < order.length(); i++){
+            map.put(order.charAt(i), index--);
+        }
+        for(int i = 0; i < words.length - 1; i++){
+            if(!f(words[i], words[i + 1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean f(String str1, String str2){
+        if(str1.equals(str2)){
+            return true;
+        }
+        int left = 0;
+        int right = 0;
+        while(left < str1.length() && right < str2.length()){
+            if(map.get(str1.charAt(left)) < map.get(str2.charAt(right))){
+                return false;
+            }else if(map.get(str1.charAt(left)) == map.get(str2.charAt(right))){
+                left++;
+                right++;
+            }else if(map.get(str1.charAt(left)) > map.get(str2.charAt(right))){
+                return true;
+            }
+        }
+        if(left < str1.length()){
+            return false;
+        }
+        return true;
+    }
+}
