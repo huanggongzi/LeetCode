5ad76b68dbc1c74ca2e58eccd4695192e2a81486
hls1793929520 <1793929520@qq.com>
2022-06-05 23:45:05 +0800

    06月05号每日一题:478. 在圆内随机生成点,计算机操作系统

---
+++ src/com/company/每日一题/_202206005在圆内随机生成点.java
@@ -0,0 +1,30 @@
+package com.company.每日一题;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 爱敲代码的小黄
+ * @Date: 2022/06/05/23:44
+ * @Description:
+ */
+public class _202206005在圆内随机生成点 {
+    Random random;
+    double xc, yc, r;
+
+    public Solution(double radius, double x_center, double y_center) {
+        random = new Random();
+        xc = x_center;
+        yc = y_center;
+        r = radius;
+    }
+
+    public double[] randPoint() {
+        while (true) {
+            double x = random.nextDouble() * (2 * r) - r;
+            double y = random.nextDouble() * (2 * r) - r;
+            if (x * x + y * y <= r * r) {
+                return new double[]{xc + x, yc + y};
+            }
+        }
+    }
+}
